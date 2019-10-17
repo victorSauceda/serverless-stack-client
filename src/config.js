4681eb6de0528942a58f843bbfd4fb80f4cc864e
victorSauceda <53831271+victorSauceda@users.noreply.github.com>
2019-10-17 11:03:02 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notesbucket89"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://tvgsps0fxd.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_taGyB2LD9",
+        APP_CLIENT_ID: "6a53bagsbm8rp53h6k4turodm7",
+        IDENTITY_POOL_ID: "us-east-2:dfd81b05-91ba-49e1-87e0-5c53068f2ec7"
+    }
+};
